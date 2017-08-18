f82cbae7405fe9bd584655adc3f622da5d3cb415
Hawken Ritter <hawkenritter@gmail.com>
2017-08-18 14:53:43 -0700

    AJax question page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,57 @@
 get '/question/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers
-  # @comments = @answers.comments
+
   erb :'show'
 end
 
 
-#should probably go in an answer controller
-# post '/answer/:id/vote' do
-#   post = Answer.find(params[:id])
-#   post.votes.create(value: 1)
-#   redirect "/questions" #what is the route for this
+post '/question/:id' do
+  #   @question = Question.find(params[:id])
+  #   @new_answer = Answer.create(description: params[:description], question_id: @question.id, answerer_id: session[:id])
+  # if @new_answer.save
+  #   status 200
+  #   if request.xhr?
+  #     @new_answer
+  #   else
+  #     #redirect somewhere?
+  #   end
+  # else
+  #   #Throw error
+  # end
+end
 
-#   #still need down vote.. maybe a whole other route??
-# end
+post '/questions/:id/answers' do
+    @question = Question.find(params[:id])
+    @new_answer = Answer.create(description: params[:description], question_id: @question.id, answerer_id: session[:id])
+  if @new_answer.save
+    status 200
+    if request.xhr?
+      erb :'_answers', layout: false, locals: {answer: @new_answer}
+    else
+      redirect "/questions/#{@question.id}"
+    end
+  else
+    status 422
+  end
+end
+
+get '/answers/:id/comments' do
+  @comment = Comment.create( )
+end
+
+get '/questions/:id' do
+end
+
+get '/questions/new' do
+end
+
+
+
+post '/answer/:id/vote' do
+  post = Answer.find(params[:id])
+  post.votes.create(value: 1)
+  redirect "/questions" #what is the route for this
+
+  #still need down vote.. maybe a whole other route??
+end
--- public/js/application.js
+++ public/js/application.js
@@ -4,7 +4,30 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-  var new_answer = function() {
-    $
-  }
+
+  new_answer();
 });
+
+
+var new_answer = function() {
+    $('.new-answer').on('submit', function(){
+      event.preventDefault();
+
+      console.log(this)
+      console.log('STOP REDIRECT')
+      var $new_answer = $(this).serialize()
+      var $url = $(this).attr('action')
+      console.log($url)
+
+
+      $.ajax( {
+        method: "POST",
+        url: $url,
+        data: $new_answer
+      })
+      .done(function(response){
+        console.log(response)
+        $('#answers').append(response);
+      });
+    });
+  };
