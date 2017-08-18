df7df9f4fb07b41930ee3970046421c26bc4912a
Hawken Ritter <hawkenritter@gmail.com>
2017-08-18 12:24:18 -0700

    make shell for question answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,16 @@
 get '/question/:id' do
   @question = Question.find(params[:id])
-  @answers = @questoin.answers
-  @comments = @answers.comments
+  @answers = @question.answers
+  # @comments = @answers.comments
   erb :'show'
 end
+
+
+#should probably go in an answer controller
+# post '/answer/:id/vote' do
+#   post = Answer.find(params[:id])
+#   post.votes.create(value: 1)
+#   redirect "/questions" #what is the route for this
+
+#   #still need down vote.. maybe a whole other route??
+# end
