9bafa47a5ce40ad1dcc9848940c31395abac6dc6
gloriacruz <ally.cruz@gmail.com>
2017-08-18 11:44:50 -0700

    Add basic CSS styling

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,9 @@ class Question < ActiveRecord::Base
 
   validates :author_id, presence: { message: "must have an author_id" }
 
+  validates :title, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+
   def self.most_recent
     Question.order(created_at: :desc).limit(15)
   end
